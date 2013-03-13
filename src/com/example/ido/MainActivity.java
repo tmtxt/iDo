fa56955ce2a76641f142b548d5322d87006f8d72
Tran Xuan Truong <tmtxt@MacBook-Pro.local>
2013-03-13 10:51:14 +0700

    modify list view action listener

--- src/com/example/ido/MainActivity.java
+++ src/com/example/ido/MainActivity.java
@@ -12,12 +12,12 @@ import android.widget.ListView;
 public class MainActivity extends Activity {
 
 	private ListView mainListView;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		mainListView = (ListView) findViewById(R.id.activity_main_options_listview);
 		mainListView.setOnItemClickListener(new OnItemClickListener() {
 
@@ -25,17 +25,30 @@ public class MainActivity extends Activity {
 			public void onItemClick(AdapterView<?> adapterView, View listView, int selectedItemId,
 					long arg4) {
 				// TODO Auto-generated method stub
-				showAllGroups(adapterView, selectedItemId);
+				mainListViewOnItemClickHandler(adapterView, listView, selectedItemId, arg4);
 			}
 		});
 	}
-	
-	private void showAllGroups(AdapterView<?> adapterView, int selectedItemId){
+
+	private void mainListViewOnItemClickHandler(AdapterView<?> adapterView, View listView, int selectedItemId,
+			long arg4) {
 		String selectedItem = (String) mainListView.getItemAtPosition(selectedItemId);
 		if(selectedItem.equals(getString(R.string.activity_main_options_list_show_all_groups))){
-			Intent intent = new Intent(this, ViewAllGroupsActivity.class);
-			startActivity(intent);
+			showAllGroups();
 		}
+		if(selectedItem.equals(getString(R.string.activity_main_options_list_show_all_tasks))){
+			showAllTasks();
+		}
+	}
+	
+	private void showAllTasks(){
+		Intent showAllTasksIntent = new Intent(this, ViewAllTasksActivity.class);
+		startActivity(showAllTasksIntent);
+	}
+
+	private void showAllGroups(){
+		Intent showAllGroupsIntent = new Intent(this, ViewAllGroupsActivity.class);
+		startActivity(showAllGroupsIntent);
 	}
 
 	@Override
