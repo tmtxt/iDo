23ab3ae85b91e9b6d4465c988989d55001b1f563
Tran Xuan Truong <tmtxt@MacBook-Pro.local>
2013-03-13 10:42:42 +0700

    set action listener for show all group item in main list view to call intent to ViewAllGroupsActivity

--- src/com/example/ido/MainActivity.java
+++ src/com/example/ido/MainActivity.java
@@ -2,14 +2,40 @@ package com.example.ido;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
+	private ListView mainListView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mainListView = (ListView) findViewById(R.id.activity_main_options_listview);
+		mainListView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> adapterView, View listView, int selectedItemId,
+					long arg4) {
+				// TODO Auto-generated method stub
+				showAllGroups(adapterView, selectedItemId);
+			}
+		});
+	}
+	
+	private void showAllGroups(AdapterView<?> adapterView, int selectedItemId){
+		String selectedItem = (String) mainListView.getItemAtPosition(selectedItemId);
+		if(selectedItem.equals(getString(R.string.activity_main_options_list_show_all_groups))){
+			Intent intent = new Intent(this, ViewAllGroupsActivity.class);
+			startActivity(intent);
+		}
 	}
 
 	@Override
