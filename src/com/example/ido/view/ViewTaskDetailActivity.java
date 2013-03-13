7019b590fb80e26618aa6d80d2009030e332760e
Tran Xuan Truong <tmtxt@MacBook-Pro.local>
2013-03-13 21:43:27 +0700

    action listener for add new task menu button

--- src/com/example/ido/view/ViewTaskDetailActivity.java
+++ src/com/example/ido/view/ViewTaskDetailActivity.java
@@ -3,10 +3,12 @@ package com.example.ido.view;
 import com.example.ido.R;
 import com.example.ido.R.layout;
 import com.example.ido.R.menu;
+import com.example.ido.controller.ApplicationNavigationHandler;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class ViewTaskDetailActivity extends GeneralActivity {
 
@@ -21,6 +23,17 @@ public class ViewTaskDetailActivity extends GeneralActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.view_task_detail, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()){
+		case R.id.activity_view_task_detail_Menu_actionbar_Item_add:
+			ApplicationNavigationHandler.addNewTask(this);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 
 }
