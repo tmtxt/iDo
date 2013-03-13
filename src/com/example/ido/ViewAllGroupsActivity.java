6e4e68cda05917e93fe6ad32ebcc33acb80cac16
Tran Xuan Truong <tmtxt@MacBook-Pro.local>
2013-03-13 12:05:49 +0700

    add action listener for Add New Group button

--- src/com/example/ido/ViewAllGroupsActivity.java
+++ src/com/example/ido/ViewAllGroupsActivity.java
@@ -3,7 +3,9 @@ package com.example.ido;
 import android.os.Bundle;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class ViewAllGroupsActivity extends GeneralActivity {
 
@@ -11,7 +13,7 @@ public class ViewAllGroupsActivity extends GeneralActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_view_all_groups);
-		
+
 	}
 
 	@Override
@@ -19,6 +21,17 @@ public class ViewAllGroupsActivity extends GeneralActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.view_all_groups, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()){
+		case R.id.view_all_groups_activity_add_group:
+			ApplicationNavigationHandler.addNewGroup(this);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 
 }
