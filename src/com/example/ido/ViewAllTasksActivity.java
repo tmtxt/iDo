0a1806b709d64cbc25462fd68705e73d03060437
Tran Xuan Truong <tmtxt@MacBook-Pro.local>
2013-03-13 10:50:54 +0700

    add ViewAllTasksActivity

---
+++ src/com/example/ido/ViewAllTasksActivity.java
@@ -0,0 +1,22 @@
+package com.example.ido;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ViewAllTasksActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_view_all_tasks);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.view_all_tasks, menu);
+		return true;
+	}
+
+}
