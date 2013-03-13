9ecb49a96956122a8a0ed15341724626ca3bb132
Tran Xuan Truong <tmtxt@macbook-pro.rmit.edu.vn>
2013-03-13 14:44:51 +0700

    add ViewTaskDetailActivity

---
+++ src/com/example/ido/ViewTaskDetailActivity.java
@@ -0,0 +1,22 @@
+package com.example.ido;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ViewTaskDetailActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_view_task_detail);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.view_task_detail, menu);
+		return true;
+	}
+
+}
