531d7c463d094e235996bd5dc4b364ebe2d829e4
Tran Xuan Truong <tmtxt@MacBook-Pro.local>
2013-03-13 10:11:41 +0700

    add view all groups activity

---
+++ src/com/example/ido/ViewAllGroupsActivity.java
@@ -0,0 +1,22 @@
+package com.example.ido;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ViewAllGroupsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_view_all_groups);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.view_all_groups, menu);
+		return true;
+	}
+
+}
