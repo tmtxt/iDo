159de6b9dd18c411c72a402021de5ad0d02be788
Tran Xuan Truong <tmtxt@MacBook-Pro.local>
2013-03-14 13:30:26 +0700

    add editExistingGroup function

--- src/com/example/ido/controller/ApplicationNavigationHandler.java
+++ src/com/example/ido/controller/ApplicationNavigationHandler.java
@@ -1,5 +1,6 @@
 package com.example.ido.controller;
 
+import com.example.ido.model.Group;
 import com.example.ido.view.ModifyGroupActivity;
 import com.example.ido.view.ModifyTaskActivity;
 import com.example.ido.view.ViewAllGroupsActivity;
@@ -8,6 +9,7 @@ import com.example.ido.view.ViewTaskDetailActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 
 // A class consists of static functions that help the application navigation between activity
 // The idea of the class is that many events can call to the same activity,
@@ -34,10 +36,22 @@ public class ApplicationNavigationHandler {
 		sourceActivity.startActivity(showAllTasksIntent);
 	}
 	
-	// Go to ModifyGroupActivity
+	// Go to ModifyGroupActivity to add a new group
 	public static void addNewGroup(Activity sourceActivity){
 		Intent addNewGroupIntent = new Intent(sourceActivity, ModifyGroupActivity.class);
 		sourceActivity.startActivity(addNewGroupIntent);
+	}
+	
+	// Go to ModifyGroupActivity to edit an existing group
+	public static void editExistingGroup(Activity sourceActivity, Group existingGroup){
+		Intent editExistingGroupIntent = new Intent(sourceActivity, ModifyGroupActivity.class);
+		// put the group to edit into bundle
+		Bundle editExistingGroupBundle = new Bundle();
+		editExistingGroupBundle.putSerializable(Group.GROUP_BUNDLE_KEY, existingGroup);
+		// put the bundle into intent
+		editExistingGroupIntent.putExtras(editExistingGroupBundle);
+		// start the activity
+		sourceActivity.startActivity(editExistingGroupIntent);
 	}
 	
 	// Go to ModifyTaskActivity
