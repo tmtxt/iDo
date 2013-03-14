cd82be49d3b9e1d6f72343dc6f2bc428367f5685
Tran Xuan Truong <tmtxt@MacBook-Pro.local>
2013-03-14 13:32:04 +0700

    check if ModifyGroupActivity is going to create a new group or edit an existing one

--- src/com/example/ido/view/ModifyGroupActivity.java
+++ src/com/example/ido/view/ModifyGroupActivity.java
@@ -4,18 +4,57 @@ import com.example.ido.R;
 import com.example.ido.R.layout;
 import com.example.ido.R.menu;
 import com.example.ido.controller.ConfirmCancelDialogHandler;
+import com.example.ido.model.Group;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
+// This activity is used for 2 purposes: Creating new Group and Editing existing Group
+// Every time it's loaded, the program should check whether the Group object in the bundle is exist
+// If it's exist, this means the activity is going to edit that group
+// Otherwise, the activity is going to create a new group
 public class ModifyGroupActivity extends GeneralActivity {
 
+	// a Group object to hold the data about the current group being processed
+	// If the activity is creating a new group, this object will be null
+	// Otherwise, it will be retrive from the bundle
+	private Group group = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_modify_group);
+		
+		// Check whether this activity is going to create a new Group or edit an existing group
+		// First, retrieve the Group object from the bundle
+		Bundle modifyGroupBundle = this.getIntent().getExtras();
+		try {
+			this.group = (Group) modifyGroupBundle.getSerializable(Group.GROUP_BUNDLE_KEY);
+		} catch (Exception ex){
+			ex.printStackTrace();
+		}
+		// Next, check if it is null
+		if (group != null){
+			// If the Group object exist, load data from it and put to the form
+			putDataToForm();
+			// After that, change the title of the activity to "Edit group " + group name
+		} else {
+			// If the Group object not exist, change the title of the activity to "Create new Group"
+		}
+		
+	}
+	
+	// This function is used to load data from this.group object and put it to form
+	private void putDataToForm(){
+		// First check if the group object is exist
+		if (group != null){
+			// Load group title into text field
+			EditText groupTitleEditText = (EditText) findViewById(R.id.activity_modify_group_Edittext_group_title);
+			groupTitleEditText.setText(this.group.getTitle());
+		}
 	}
 
 	@Override
