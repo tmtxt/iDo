2438c98acf70bbe6e71144e1952f59e0ff6419ca
Tran Xuan Truong <tranxuantruong@MacBook-Pro.local>
2013-03-21 10:06:15 +0700

    add SettingActivity

--- src/com/example/ido/view/SettingActivity.java
+++ src/com/example/ido/view/SettingActivity.java
@@ -7,6 +7,7 @@ import com.example.ido.R.menu;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class SettingActivity extends GeneralActivity {
 
@@ -14,6 +15,17 @@ public class SettingActivity extends GeneralActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_setting);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()){
+		case R.id.activity_setting_Menu_actionbar_Item_cancel:
+			this.finish();
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+
 	}
 
 	@Override
