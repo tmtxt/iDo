44bafb83c2e1449d3ceec35b53d1336fdf247638
Tran Xuan Truong <tranxuantruong@MacBook-Pro.local>
2013-03-21 17:20:10 +0700

    add DatabaseAdapter class

---
+++ src/com/example/ido/model/DatabaseAdapter.java
@@ -0,0 +1,89 @@
+package com.example.ido.model;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+// This class is used for interacting with database
+public class DatabaseAdapter {
+	
+	// STATIC VARIABLES FOR DEFINING DATABASE
+	
+	// Used for logging
+	public static final String TAG = "ido";
+	
+	private DatabaseHelper databaseHelper;
+	private SQLiteDatabase sqLiteDatabase;
+	private final Context context;
+	
+	// Database name
+	public static final String DATABASE_NAME = "ido_task_management.db";
+	
+	// Database version
+	public static final int DATABASE_VERSION = 2;
+	
+	// Group table
+	// Group table name
+	public static final String GROUP_TABLE_NAME = "task_group";
+	// Group table - id column name
+	public static final String GROUP_TABLE_COLUMN_ID = "_id";
+	// Group table - title coumn name
+	public static final String GROUP_TABLE_COULMN_TITLE = "_title";
+	// Group table create statement
+	private static final String GROUP_TABLE_CREATE
+			= "create table " + GROUP_TABLE_NAME
+			+ " ( "
+			+ GROUP_TABLE_COLUMN_ID + " text primary key, "
+			+ GROUP_TABLE_COULMN_TITLE + " text not null "
+			+ " );";
+	
+	
+	// STATIC CLASS DATABASE HELPER
+	
+	private static class DatabaseHelper extends SQLiteOpenHelper{
+		
+		// Must override implicit constructor
+		public DatabaseHelper(Context context, String name, CursorFactory factory, int version){
+			super(context, name, factory, version);
+		}
+
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			// TODO Auto-generated method stub
+			db.execSQL(DatabaseAdapter.GROUP_TABLE_CREATE);
+			Log.i(TAG, "table created");
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			// TODO Auto-generated method stub
+			db.execSQL("Drop table if exists task_group");
+		}
+		
+	}
+	
+	
+	// UTILITY FUNCTIONS FOR CREATING DATABASE AND MANIPULATING DATA
+	
+	// Constructor, pass the current activity to the context
+	public DatabaseAdapter(Context context){
+		this.context = context;
+	}
+	
+	// Open connection to database, should be called right after constructor
+	public DatabaseAdapter open(){
+		databaseHelper = new DatabaseHelper(context, this.DATABASE_NAME, null, this.DATABASE_VERSION);
+		sqLiteDatabase = databaseHelper.getWritableDatabase();
+		return this;
+	}
+	
+	// Close connection to database, should be called at the end when everything is finished
+	public void close(){
+		databaseHelper.close();
+	}
+	
+	
+	
+}
